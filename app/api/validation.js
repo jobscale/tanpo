46c9a7bbad06dd28145f65d7200bbe00fc39cd7c
jobscale <jobscale@users.noreply.github.com>
2022-12-28 13:14:05 +0900

    verify updated

--- app/api/validation.js
+++ app/api/validation.js
@@ -5,12 +5,15 @@ class ApiValidation {
     const { body } = req;
     const { error } = Joi.object({
       text: Joi.string().required().min(1).max(2 ** 16 - 1),
+      channel: Joi.string(),
       icon_emoji: Joi.string(),
       username: Joi.string(),
       attachments: Joi.any(),
     }).validate(body);
     if (error) {
-      res.status(400).json({ message: error.message });
+      res.status(400).json({
+        message: `${error.message} verify`,
+      });
       return;
     }
     next();
