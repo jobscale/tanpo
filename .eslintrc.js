3306cf04d1cca4a66eaeb99524f0d41c6d1f6ac2
jobscale <jobscale@users.noreply.github.com>
2025-11-26 04:21:08 +0900

    eslint revert

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+  },
+  rules: {
+    indent: ['error', 2, { MemberExpression: 0 }],
+    'no-trailing-spaces': 'error',
+    'arrow-parens': 'off',
+    'no-plusplus': 'off',
+    'class-methods-use-this': 'off',
+    'no-await-in-loop': 'off',
+    'no-param-reassign': 'off',
+  },
+};
